7aaea32bc80d33002452e279ba43b83a6ea47453
thummim <35131165+thummim@users.noreply.github.com>
2022-05-18 17:03:09 +0900

    Create RectangleCheck.cpp

---
+++ RectangleCheck.cpp
@@ -0,0 +1,11 @@
+bool CHafpReaderView::RectangleCheck(long startX, long startY, long endX, long endY)
+{
+	long bigX = max(startX, endX);
+	long smallX = min(startX, endX);
+
+	long bigY = max(startY, endY);
+	long smallY = min(startY, endY);
+
+	if ( ((HitCoord.x <= bigX + 30) && (HitCoord.x >= smallX - 30 )) && ((HitCoord.y <= bigY + 30 ) && (HitCoord.y >= smallY - 30 )) )	return true;
+	else return false;
+}
