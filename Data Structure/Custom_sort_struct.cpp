8d25f67e6c40cc2fdbc0f26ad12c31bfa382ed06
Thummim Lee <35131165+thummim@users.noreply.github.com>
2022-06-17 15:12:10 +0900

    Create Custom_sort_struct.cpp

---
+++ Data Structure/Custom_sort_struct.cpp
@@ -0,0 +1,9 @@
+struct Point
+{
+  int y, x;
+  Point(int y, int x) : y(y), x(x){}
+  Point(){y = -1; x = -1; }
+  bool operator < (const Point & a) const{
+  if(x == a.x) return y < a.y;
+  return x < a.x;
+}
